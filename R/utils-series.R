0e111f102c8a09f65978b67fbd62a1c79e32781e
CamilaAchuri <camilaachury.4@gmail.com>
2024-02-12 13:51:41 -0300

    template series function

---
+++ R/utils-series.R
@@ -0,0 +1,19 @@
+#' @export
+hc_data_series <- function(hc, data) {
+
+  is_list_of_series <- is.list(data)
+  list_in_list <- all(sapply(data, function(x) is.list(x) && !is.null(x$data)))
+
+  # Si los datos son una lista de series, utiliza hc_add_series_list o hc_series dependiendo de la estructura
+  if (is_list_of_series) {
+    if (list_in_list) {
+      return(hc_add_series_list(hc, data))
+    } else {
+      # Si es una única serie pero aún así es una lista, utiliza hc_series
+      return(hc_series(hc, data))
+    }
+  } else {
+    # Para un único conjunto de datos que no está dentro de una lista de series
+    return(hc_add_series(hc, data))
+  }
+}
---
+++ tests/testthat/test-hc_data_series.R
@@ -0,0 +1,30 @@
+test_that("hc_data_series", {
+
+
+  data_list <- list(
+    name = "London",
+    data = c(3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8)
+  )
+
+  highchart() |> hc_data_series(data_list)
+
+
+  data_list <- list(
+    list(
+      name = "Tokyo",
+      data = c(7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6)
+    ),
+    list(
+      name = "London",
+      data = c(3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8)
+    )
+  )
+
+  highchart() |> hc_data_series(data_list)
+
+
+  data <- abs(rnorm(5))
+  highchart() |> hc_data_series(data)
+
+
+})

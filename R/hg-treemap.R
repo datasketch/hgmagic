fa70c8577d46f36c823e970b5caa92b29290656f
CamilaAchuri <camilaachury.4@gmail.com>
2024-02-26 20:06:24 -0300

    test treemap

--- R/hg-treemap.R
+++ R/hg-treemap.R
@@ -15,13 +15,8 @@ hg_treemap <- function(data,
   var_cat <- c(var_cat, var_yea)
   data_viz <- data_prep(ht$data, ht$dic, var_cat, var_num, ...)
 
-  color_by <- var_cat
-  if (length(var_cat) > 1) {
-    color_by <- var_cat[1]
-    data_viz <- completevalues(data_viz, var_find = var_cat[1],
-                               var_expand = var_cat[2], var_num = var_num)
-  }
-  if (length(var_num) > 1) color_by <- var_cat
+  color_by <- var_cat[1]
+
   data_viz <- colors_data(data_viz, color_by = color_by, ...)
   data_viz <- hg_list(data_viz, hdtype, "treemap")
 
--- tests/testthat/test-hc_treemap.R
+++ tests/testthat/test-hc_treemap.R
@@ -1,4 +1,9 @@
 test_that("hc_treemap", {
   hg_treemap(data = iris, var_num = "sepal_length", var_cat = "species")
   hg_treemap(data = starwars, var_cat = c("sex", "hair_color"), datalabel_show = T)
+
+  data_sample <- sample_data("Cat-Num", names = c("cat", "value"))
+  hg_treemap_CatNum(data_sample)
+  data_sample <- sample_data("Cat-Cat-Num", names = c("cat", "cat2", "value"))
+  hg_treemap_CatCatNum(data_sample)
 })

c9e6e8e8fb06af63d950b537b634cae2349bfe56
CamilaAchuri <camilaachury.4@gmail.com>
2024-07-04 18:01:57 -0300

    add prefix and suffix options

--- R/utils-axis.R
+++ R/utils-axis.R
@@ -6,13 +6,19 @@ hc_axis <- function(hc, axis = "x", categories = NULL, type = NULL, opts) {
 
   axis_function <- if (axis == "x") hc_xAxis else hc_yAxis
   axis_title <- if (axis == "x") opts$title_axis_x else opts$title_axis_y
-
+  axis_labels <- NULL
+  if (axis == "y") {
+    axis_labels <-  paste0(opts$prefix, "{value}", opts$suffix)
+  }
 
   hc |>
     axis_function(
       categories = categories,
       crossing = NULL,
       endOnTick = TRUE,
+      labels = list(
+        format = axis_labels
+      ),
       title = list(
         style = list(
           color = "#666666",

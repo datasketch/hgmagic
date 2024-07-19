b23f32c769002848f402a116e2bb727e60bfdc4e
CamilaAchuri <camilaachury.4@gmail.com>
2024-07-19 18:13:34 -0300

    add default color from data in items plot

--- R/hg-items.R
+++ R/hg-items.R
@@ -1,9 +1,9 @@
 #' @export
 hg_item <- function(data,
-                   dic = NULL,
-                   var_cat = NULL,
-                   var_yea = NULL,
-                   var_num = NULL, ...) {
+                    dic = NULL,
+                    var_cat = NULL,
+                    var_yea = NULL,
+                    var_num = NULL, ...) {
 
 
   if (is.null(data)) stop("You must enter a dataset")
@@ -15,8 +15,13 @@ hg_item <- function(data,
   var_cat <- c(var_cat, var_yea)
   data_viz <- data_prep(ht$data, ht$dic, var_cat, var_num, ...)
 
-  color_by <- var_cat[1]
-  data_viz <- colors_data(data_viz, color_by = color_by, ...)
+  if ("..colors" %in% names(data)) {
+    data_colors <- data |> distinct_(var_cat, "..colors")
+    data_viz <- data_viz |> left_join(data_colors, by = var_cat)
+  } else {
+    color_by <- var_cat[1]
+    data_viz <- colors_data(data_viz, color_by = color_by, ...)
+  }
 
   data_viz <- hg_list(data_viz, hdtype, "item")
 
